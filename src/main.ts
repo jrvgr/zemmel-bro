a7edc891f6a3b860ce1e175776ed461317541c07
viriv <viriv@protonmail.com>
2022-04-16 10:49:31 +0200

    (try) fix(ing) the 500 issue on load

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,7 @@ import dayjs from "dayjs";
 import isoWeek from "dayjs/plugin/isoWeek";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 import updateLocale from "dayjs/plugin/updateLocale";
-import {
-  api,
-  getallsubjects,
-  getStudentInfo,
-  getTeachers,
-  studentSchoolYears,
-} from "@/api";
+import { api, studentSchoolYears } from "@/api";
 import "./style.scss";
 import App from "./App.svelte";
 
@@ -38,26 +32,18 @@ function restorescheme() {
 
 restorescheme();
 
-// studentSchoolYears("~me").then((data) => {
-//   console.log(data.data.response.data[0]);
+// studentSchoolYears("~me").then((year) => {
+//   api
+//     .get("subjectselectionsubjects", {
+//       params: {
+//         fields: "code,name",
+//         schoolInSchoolYear:
+//           year.data.response.data[0].schoolInSchoolYears[
+//             year.data.response.data[0].schoolInSchoolYears.length - 1
+//           ].toString(),
+//       },
+//     })
+//     .then((data1) => {
+//       console.log("subjects", data1);
+//     });
 // });
-
-// getTeachers(year).then((data) => {
-//    console.log(data.data.response.data);
-//  });
-
-studentSchoolYears("~me").then((year) => {
-  api
-    .get("subjectselectionsubjects", {
-      params: {
-        fields: "code,name",
-        schoolInSchoolYear:
-          year.data.response.data[0].schoolInSchoolYears[
-            year.data.response.data[0].schoolInSchoolYears.length - 1
-          ].toString(),
-      },
-    })
-    .then((data1) => {
-      console.log("subjects", data1);
-    });
-});
--- src/stores.ts
+++ src/stores.ts
@@ -1,7 +1,13 @@
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import isoWeek from "dayjs/plugin/isoWeek";
 import { writable } from "svelte/store";
+
+dayjs.extend(advancedFormat);
+dayjs.extend(isoWeek);
 
 export const week = writable(0);
 export const selectedStudent = writable("~me");
-export const currentWeek = writable<string>();
+export const currentWeek = writable<string>(dayjs().format("YYYYWW"));
 export const appointments = writable([]);
 export const selectedDay = writable("Monday");
