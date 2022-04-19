3def2c03880177f8fc0fc81a263684990edca9ab
viriv <viriv@protonmail.com>
2022-04-19 11:51:15 +0200

    currentday is set to actual current day on load

--- src/stores.ts
+++ src/stores.ts
@@ -1,13 +1,17 @@
 import dayjs, { Dayjs } from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
+import localeDate from "dayjs/plugin/localeData";
+import weekday from "dayjs/plugin/weekday";
 import isoWeek from "dayjs/plugin/isoWeek";
 import { writable } from "svelte/store";
 
 dayjs.extend(advancedFormat);
 dayjs.extend(isoWeek);
+dayjs.extend(localeDate);
+dayjs.extend(weekday);
 
 export const week = writable(0);
 export const selectedStudent = writable("~me");
 export const currentWeek = writable<Dayjs>(dayjs());
 export const appointments = writable([]);
-export const selectedDay = writable<Dayjs>(dayjs().day(1));
+export const selectedDay = writable<Dayjs>(dayjs());
