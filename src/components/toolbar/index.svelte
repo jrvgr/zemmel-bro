d20335286a0badc64c7080632b77ced9a01b4745
Viriv <viriv@protonmail.com>
2022-10-01 21:12:56 +0200

    Refactor list.svelte
    Refactor!!

---
+++ src/components/toolbar/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let matches;
+  import Transition from "@/components/NavTransition.svelte";
+  import MobileCalenderNav from "./MobileCalendarNav.svelte";
+</script>
+
+{#if matches}
+  <div class="toolbar">
+    <Transition>
+      <MobileCalenderNav />
+    </Transition>
+  </div>
+{/if}
+
+<style lang="scss">
+  .toolbar {
+    position: fixed;
+    bottom: 0;
+    width: 100vw;
+    padding: 10px 0;
+    background: var(--header-background);
+  }
+</style>
